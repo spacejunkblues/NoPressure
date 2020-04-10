a7df135b8ea608e55b9887a00c492f4754829fdd
fleckm210 <fleckm210@gmail.com>
2020-04-10 01:14:47 -0600

    clone

---
+++ NoPressure/Assets/clonescript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clonescript : MonoBehaviour
+{
+
+
+
+    private Transform clonetf;
+    public Transform tf;
+    private int counter;
+    public GameObject clone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        counter = counter + 1;
+        
+
+
+        if(counter == 100)
+        {
+
+            counter = 0;
+            Instantiate(clone);
+            clonetf = clone.GetComponent<Transform>();
+            clonetf.position = tf.position;
+
+        }
+
+    }
+
+
+
+}
--- NoPressure/Assets/playerscript.cs
+++ NoPressure/Assets/playerscript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class playerscript : MonoBehaviour
 {
-
+    public Transform tf;
     public AudioSource jumpAs;
     public Rigidbody rb;
 
@@ -17,12 +17,32 @@ public class playerscript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-3, 0, 0);
+        }
 
         if(Input.GetKey(KeyCode.W))
         {
             rb.AddForce(0, 20, 0);
             Debug.Log("w");
         }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Instantiate(this);
+            Debug.Log("c");
+        }*/
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            tf.position = new Vector3((float)(tf.position.x + 0.2), tf.position.y, tf.position.z);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            tf.position = new Vector3((float)(tf.position.x - 0.2), tf.position.y, tf.position.z);
+        }
+
     }
     void OnCollisionEnter(Collision collisionInfo)
     {

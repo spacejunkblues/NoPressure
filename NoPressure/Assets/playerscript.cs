3189d32bcc9ad526a6aab57024545e0bacc32b65
fleckm210 <fleckm210@gmail.com>
2020-04-09 23:47:03 -0600

    added player

---
+++ NoPressure/Assets/playerscript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerscript : MonoBehaviour
+{
+
+    public AudioSource jumpAs;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(0, 20, 0);
+            Debug.Log("w");
+        }
+    }
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+
+
+
+        jumpAs.Play();
+
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # NoPressure
+# marshy note

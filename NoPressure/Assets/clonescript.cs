dc764c0ff8ed8bf454a567cee98d3b90675198ce
fleckm210 <fleckm210@gmail.com>
2020-04-12 18:49:09 -0600

    clone

--- NoPressure/Assets/clonescript.cs
+++ NoPressure/Assets/clonescript.cs
@@ -6,44 +6,55 @@ public class clonescript : MonoBehaviour
 {
 
 
-
-    private Transform clonetf;
-    public Transform tf;
     private int counter;
     public GameObject clone;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        counter = 0;
-
+        print(gameObject.name);
+        
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    private void FixedUpdate()
+    void FixedUpdate()
     {
 
         counter = counter + 1;
-        
 
-
-        if(counter == 100)
+        if(counter == 250)
         {
 
-            counter = 0;
-            Instantiate(clone);
-            clonetf = clone.GetComponent<Transform>();
-            clonetf.position = tf.position;
+            Destroy(gameObject);
 
         }
 
+
+        rb.AddForce(0, 0, -5);
+
+        
+
+    }
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+
+        if (collisionInfo.collider.name == "player")
+        {
+
+
+            Destroy(gameObject);
+            
+
+        }
     }
 
 
-
 }
+
+
+
+
+
+
+

51802bd47843c754ab61fe74a249aa4ecbf91b6a
fleckm210 <fleckm210@gmail.com>
2020-04-15 12:20:53 -0600

    fix

---
+++ lesson1/lesson1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lesson1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            // this is to demonstrate the difference between these different lines of code
+            // console.write is helpful for writing multiple things in one line
+            Console.Write("hello");
+
+            //while console.writeline is useful for conveying structure
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+
+
+
+
+        }
+    }
+}

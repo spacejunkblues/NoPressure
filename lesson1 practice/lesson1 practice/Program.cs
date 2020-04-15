b23aa5e384b021fddb415455ccd3189d48e47c24
Dude <spacejunkblues@gmail.com>
2020-04-14 17:19:48 -0700

    basic code

---
+++ lesson1 practice/lesson1 practice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace lesson1_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Writes to console
+            Console.Write("hello");
+
+            //Writes to console and enters
+            Console.WriteLine("Will this work?");
+        }
+    }
+}

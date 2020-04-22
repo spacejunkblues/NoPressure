546ddd23edbaebc6bb5cf1bd7c86e2e20d6dd792
fleckm210 <fleckm210@gmail.com>
2020-04-22 13:31:09 -0600

    project lesson 3 added

---
+++ lesson3/lesson3/Program.cs
@@ -0,0 +1,51 @@
+﻿//marshal fleck
+//april 22 2020
+//this an assignment given to 
+//me by the greatest uncle on the face of planet earth. maybe even the galaxy. theres no telling, really.
+//
+//
+using System;
+
+namespace lesson3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int username1;
+            int username2;
+            int username3;
+            int username4;
+            int username5;
+
+            username1 = 0;
+            username2 = 0;
+            username3 = 0;
+            username4 = 0;
+            username1 = 0;
+
+
+            Console.WriteLine("what is your name?");
+            username1 = Console.Read();
+            username2 = Console.Read();
+            username3 = Console.Read();
+            username4 = Console.Read();
+            username5 = Console.Read();
+
+            Console.Write(username1);
+            Console.Write(username2);
+            Console.Write(username3);
+            Console.Write(username4);
+            Console.Write(username5);
+            Console.Write(",");
+            Console.Write("huh?");
+            Console.Write("    hmmmm,");
+            Console.WriteLine(" i think i knew somebody with that name.");
+            Console.Write(" fascinating,");
+            Console.Write(" isn't it?");
+            Console.WriteLine(" ");
+          
+
+        }
+    }
+}

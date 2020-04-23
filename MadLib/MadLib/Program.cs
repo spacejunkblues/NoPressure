6fbbf23e9a171fd2ea2b9d9e746ca6bc8e976bb4
fleckm210 <fleckm210@gmail.com>
2020-04-23 13:22:09 -0600

    worked on madlib

---
+++ MadLib/MadLib/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace MadLib
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int userinput1;
+            int userinput2;
+            int userinput3;
+            int userinput4;
+            int userinput5;
+            int userinput6;
+            int userinput7;
+            int userinput8;
+            int userinput9;
+            int userinput10;
+            int userinput11;
+            int userinput12;
+            int userinput13;
+            int userinput14;
+            int userinput15;
+            int userinput16;
+            int userinput17;
+            int userinput18;
+            int userinput19;
+            int userinput20;
+            int userinput21;
+            int userinput22;
+            int userinput23;
+            int userinput24;
+            int userinput25;
+            int userinput26;
+            int userinput27;
+            int userinput28;
+            int userinput29;
+            int userinput30;
+            int userinput31;
+            int userinput32;
+            int userinput33;
+            int userinput34;
+            int userinput35;
+            int userinput36;
+            int userinput37;
+            int userinput38;
+            int userinput39;
+            int userinput40;
+            int userinput41;
+            int userinput42;
+            int userinput43;
+            int userinput44;
+            int userinput45;
+            int userinput46;
+            int userinput47;
+            int userinput48;
+
+
+            Console.WriteLine("noun:");
+            userinput1 = Console.Read();
+            userinput2 = Console.Read();
+            userinput3 = Console.Read();
+            userinput4 = Console.Read();
+            userinput5 = Console.Read();
+            userinput6 = Console.Read();
+            userinput7 = Console.Read();
+            userinput8 = Console.Read();
+            userinput9 = Console.Read();
+            userinput10 = Console.Read();
+            Console.WriteLine(" ");
+            Console.WriteLine("verb:");
+            userinput21 = Console.Read();
+            userinput22 = Console.Read();
+            userinput23 = Console.Read();
+            userinput24 = Console.Read();
+            userinput25 = Console.Read();
+            userinput26 = Console.Read();
+            userinput27 = Console.Read();
+            userinput28 = Console.Read();
+            userinput29 = Console.Read();
+            userinput30 = Console.Read();
+
+            Console.WriteLine(" ");
+            Console.WriteLine("verb:");
+            userinput11 = Console.Read();
+            userinput12 = Console.Read();
+            userinput13 = Console.Read();
+            userinput14 = Console.Read();
+            userinput15 = Console.Read();
+            userinput16 = Console.Read();
+            userinput17 = Console.Read();
+            userinput18 = Console.Read();
+            userinput19 = Console.Read();
+            userinput20 = Console.Read();
+            Console.WriteLine(" ");
+
+            Console.Write((char)userinput1);
+            Console.Write((char)userinput2);
+            Console.Write((char)userinput3);
+            Console.Write((char)userinput4);
+            Console.Write((char)userinput5);
+            Console.Write((char)userinput6);
+            Console.Write((char)userinput7);
+            Console.Write((char)userinput8);
+            Console.Write((char)userinput9);
+            Console.Write((char)userinput10);
+            Console.Write("was ");
+
+            Console.Write((char)userinput21);
+            Console.Write((char)userinput22);
+            Console.Write((char)userinput23);
+            Console.Write((char)userinput24);
+            Console.Write((char)userinput25);
+            Console.Write((char)userinput26);
+            Console.Write((char)userinput27);
+            Console.Write((char)userinput28);
+            Console.Write((char)userinput29);
+            Console.Write((char)userinput30);
+
+            Console.Write("when he ");
+            Console.Write((char)userinput11);
+            Console.Write((char)userinput12);
+            Console.Write((char)userinput13);
+            Console.Write((char)userinput14);
+            Console.Write((char)userinput15);
+            Console.Write((char)userinput16);
+            Console.Write((char)userinput17);
+            Console.Write((char)userinput18);
+            Console.Write((char)userinput19);
+            Console.Write((char)userinput10);
+
+
+
+        }
+    }
+}

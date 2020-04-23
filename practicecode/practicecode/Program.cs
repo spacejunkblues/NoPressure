85889007e1ccfd0bb6d8d8eb6d66265391513edf
fleckm210 <fleckm210@gmail.com>
2020-04-23 09:14:25 -0600

    fixed practicecode-Debug|anycpu.json

--- practicecode/practicecode/Program.cs
+++ practicecode/practicecode/Program.cs
@@ -18,18 +18,69 @@ namespace practicecode
             int userinput4;
             int userinput5;
             int userinput6;
+            int userinput7;
+            int userinput8;
+            int userinput9;
+            int userinput10;
+            int userinput11;
+            int userinput12;
+            int userinput13;
+            int userinput14;
+            int userinput15;
+            int userinput16;
+            int userinput17;
+            int userinput18;
+            int userinput19;
+            int userinput20;
 
 
-            userinput1 = 0;
 
 
             Console.WriteLine("type the word you want to show backwards:");
             userinput1 = Console.Read();
+            userinput2 = Console.Read();
+            userinput3 = Console.Read();
+            userinput4 = Console.Read();
+            userinput5 = Console.Read();
+            userinput6 = Console.Read();
+            userinput7 = Console.Read();
+            userinput8 = Console.Read();
+            userinput9 = Console.Read();
+            userinput10 = Console.Read();
+            userinput11 = Console.Read();
+            userinput12 = Console.Read();
+            userinput13 = Console.Read();
+            userinput14 = Console.Read();
+            userinput15 = Console.Read();
+            userinput16 = Console.Read();
+            userinput17 = Console.Read();
+            userinput18 = Console.Read();
+            userinput19 = Console.Read();
+            userinput20 = Console.Read();
 
 
+            Console.WriteLine(" ");
 
-
-
+            Console.Write((char)userinput20);
+            Console.Write((char)userinput19);
+            Console.Write((char)userinput18);
+            Console.Write((char)userinput17);
+            Console.Write((char)userinput16);
+            Console.Write((char)userinput15);
+            Console.Write((char)userinput14);
+            Console.Write((char)userinput13);
+            Console.Write((char)userinput12);
+            Console.Write((char)userinput11);
+            Console.Write((char)userinput10);
+            Console.Write((char)userinput9);
+            Console.Write((char)userinput8);
+            Console.Write((char)userinput7);
+            Console.Write((char)userinput6);
+            Console.Write((char)userinput5);
+            Console.Write((char)userinput4);
+            Console.Write((char)userinput3);
+            Console.Write((char)userinput2);
+            Console.Write((char)userinput1);
 
 
 

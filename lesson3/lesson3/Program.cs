8fde4bfcc6cf6971fcd5a15ec657989f70e7b079
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2020-04-23 19:53:14 -0500

    added notes to lesson3 from class

--- lesson3/lesson3/Program.cs
+++ lesson3/lesson3/Program.cs
@@ -1,5 +1,6 @@
 ﻿//marshal fleck
 //april 22 2020
+//
 //this an assignment given to 
 //me by the greatest uncle on the face of planet earth. maybe even the galaxy. theres no telling, really.
 //
@@ -12,17 +13,19 @@ namespace lesson3
     {
         public static void Main(string[] args)
         {
+            //declaring varaibles
             int username1;
             int username2;
             int username3;
             int username4;
-            int username5;
+            int username5;  //this is the users fith letter in their name.
             int userlastname1;
             int userlastname2;
             int userlastname3;
             int userlastname4;
             int userlastname5;
 
+            //Init
             username1 = 0;
             username2 = 0;
             username3 = 0;
@@ -35,13 +38,28 @@ namespace lesson3
             userlastname5 = 0;
 
 
-            Console.WriteLine("what is your name? test");
+            char newline;
+
+            newline = (char)104;
+            newline = (char)10;
+            
+            //using an escape character to print "\n" to the screen
+            Console.WriteLine("\\n");
+
+
+            //Get the users first name
+            Console.WriteLine("what is your name? (type exactly 5 letters)");
             username1 = Console.Read();
             username2 = Console.Read();
             username3 = Console.Read();
             username4 = Console.Read();
             username5 = Console.Read();
 
+            //clear the buffer
+            Console.Read(); //this one will clear the 13
+            Console.Read(); //this one will clear the 10
+
+            //Greet the user
             Console.WriteLine(" ");
             Console.Write((char)username1);
             Console.Write((char)username2);
@@ -56,6 +74,8 @@ namespace lesson3
             Console.Write(" isn't it?");
             Console.WriteLine(" ");
 
+
+            //Get users lastname
             Console.WriteLine("what is your last name?");
             userlastname1 = Console.Read();
             userlastname2 = Console.Read();
@@ -80,7 +100,8 @@ namespace lesson3
             Console.Write(" is your full name, huh?");
 
 
-
+            Console.Write("\nPress any ke\ny to continue");
+            Console.ReadKey();
         }
     }
 }

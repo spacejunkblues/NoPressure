0b3129d3c9d5e3b69b632c66b3bfe8cd61281978
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2020-05-05 19:53:53 -0500

    finished loops lesson7

--- InClassLesson7_Loops/InClassLesson7_Loops/Program.cs
+++ InClassLesson7_Loops/InClassLesson7_Loops/Program.cs
@@ -8,19 +8,79 @@ namespace InClassLesson7_Loops
         {
             //declare varaibles for scope example
             int i;
-            int[] numbers = { 0,0,0};
+            string text;
+            string output;
+            int x;
 
+            //init i
             i = 0;
+            x = 2;
+            text = "i: ";
+            output = text + "||";
 
-           
+            output = text + i;
+
+            //it's the same as this
+            //output = text = (string)i;
+
+
+
             {
-                int x;
+                //int x;
                 x = 4;
                 i = 1;
             }
 
 
-            Console.WriteLine("i: " +i);
+            Console.WriteLine("i: " + i);
+
+            //same thing
+            Console.Write("x: ");
+            Console.WriteLine(x);
+
+
+            //dowhile loops
+            i = 9;
+            do
+            {
+                i++;
+            } while (i<5);
+
+
+
+            //while loop
+            i = 9;
+            while(i<5)
+            {
+                i++;
+            }
+
+
+            //for loop
+            //  (init, condition, iteration)
+            for(i=0;i<5;i++)
+            {
+
+            }
+
+
+            bool keepLooping = true;
+
+            while(keepLooping)
+            {
+                Console.WriteLine("Just entered the loop again");
+                int input;
+                input = Console.Read();
+                Console.Read();
+                Console.Read();
+
+                if (input == 104)
+                {
+                    keepLooping = false;
+                }
+
+            }
+
         }
     }
 }

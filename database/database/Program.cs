0f7ad0e0d985756ebcf60b8173998d5813da3b4c
fleckm210 <fleckm210@gmail.com>
2020-05-08 18:08:54 -0600

    worked on project

---
+++ database/database/Program.cs
@@ -0,0 +1,33 @@
+﻿//Marshal Fleck
+//Fleckm210@gmail.com
+//This is an assignment for a database that holds name and age
+using System;
+
+namespace database
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Declaring and Init database variables
+            int[] AgeArray = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            int[] NameArray = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            bool LoopActivated = true;
+
+            //introduction to the user;
+            Console.WriteLine("Hello, this is a database that can store");
+            Console.WriteLine(" Age and Name for up to 10 people.");
+            Console.WriteLine("You have entered the DataBase loop.");
+            while(LoopActivated == true)
+            {
+                Console.WriteLine("Would you like to (all Lower case)'' ");
+
+
+
+
+              
+            }
+
+        }
+    }
+}

bcb9422dddc03126431132d1f80d1f81a645fa7b
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2020-05-14 19:08:57 -0500

    finished PrimeNumber

--- PrimeNumberGenerator/PrimeNumberGenerator/Program.cs
+++ PrimeNumberGenerator/PrimeNumberGenerator/Program.cs
@@ -3,41 +3,58 @@
 //This is an assignment for a prime number generator
 using System;
 
-namespace PrimeNuberGenerator
+namespace PrimeNumberGenerator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int i = 0;
-            int[] PrimeNumbers = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
- 
+            int i = 2;
+            int LoopIndex = 0;
+            bool NotAPrimeNumber = false;
+
+            //Console.WriteLine("2");
             while (i < 100)
             {
-
-                if (i > 10)
+                
+                for (LoopIndex = 1; LoopIndex != i - 1;LoopIndex++)
                 {
 
-                    if ((i % 2) != 0 && (i % 3) != 0 && (i % 10) != 0 && (i % 5) != 0 && (i % 7) != 0)
+                    if ((i % (LoopIndex + 1)) != 0) 
                     {
+                        //might be a prime, might not be. Need to wait until end of loop
 
-                        Console.WriteLine(i);
-
+                         
+                        
+                        
+                        
+                            
+                          
                     }
-                    else if ()
+                    else if ((i % (LoopIndex + 1)) == 0) 
                     {
-
+                        //Not a prime number. FOr sure.
+                        NotAPrimeNumber = true;
                     }
-
-
-
+                       
+                    
+                    
                 }
                 
+                //If it is a prime number
+                if (!NotAPrimeNumber) 
+                {
+                    Console.WriteLine(i);
+                }
+                NotAPrimeNumber = false;
+                
 
-
-               
+                
                 i++;
+               
             }
+            Console.WriteLine("Press any key to continue:");
+            Console.ReadKey();
 
         }
     }

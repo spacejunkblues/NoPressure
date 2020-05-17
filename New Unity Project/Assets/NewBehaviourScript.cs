933292102587677e8c3a134c1a9de16fafd0b499
fleckm210 <fleckm210@gmail.com>
2020-05-17 16:43:48 -0600

    created unity project

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    //Declaring variables
+    public Rigidbody2D rb;
+    public Transform tf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            tf.Position = new vector2 (tf.Position, tf.Position + 5);
+        }
+
+    }
+}

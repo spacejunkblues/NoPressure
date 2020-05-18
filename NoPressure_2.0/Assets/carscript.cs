b708fd09f2a3b77f786ee4d223e3e156f5571b3e
fleckm210 <fleckm210@gmail.com>
2020-05-18 15:22:37 -0600

    worked on unity project

---
+++ NoPressure_2.0/Assets/carscript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carscript : MonoBehaviour
+{
+    public Transform tf;
+    public Rigidbody rb;
+    double acceleration = 0.0;
+    int i = 0;
+    public float rotation = 0;
+    public GameObject transformreset;
+    private Transform tfreset;
+        
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tfreset = GetComponent<Transform>();
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(0, 0, (float)200);
+            tf = tfreset;
+        }
+        
+    }
+}

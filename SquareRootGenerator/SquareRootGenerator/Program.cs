6fed91b25bbd018f5ae3da7ab6b9fe379187768d
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2020-05-18 21:51:43 -0500

    added squareroot project

---
+++ SquareRootGenerator/SquareRootGenerator/Program.cs
@@ -0,0 +1,63 @@
+﻿//Marshal Fleck
+//Fleckm210@gmail.com
+//This is an assignment for a Square Root Generator
+using System;
+
+namespace SquareRootGenerator
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Declaring variables
+            int UserNumber;
+            bool MainLoopActivated;
+            int i;
+            int SquareRoot;
+
+            //Init variables
+            UserNumber = 0;
+            MainLoopActivated = true;
+            i = 0;
+            SquareRoot = 0;
+
+            while (MainLoopActivated)
+            {
+
+                //Introduction to user
+                Console.WriteLine("Hello! this is a Square Root Generator.");
+                Console.WriteLine("When i ask, type the number you want to see");
+                Console.WriteLine("As a Square Root, then hit enter. :");
+
+                //Getting the Information
+                UserNumber = Convert.ToInt32(Console.ReadLine());//This converts an int to a ReadLine function
+
+
+
+
+                for (i = 0; i < UserNumber; i += 1)
+                {
+
+                    if (i * i == UserNumber)
+                    {
+
+                        //Found Square Root!
+                        SquareRoot = i;
+                        continue;
+
+
+                    }
+
+                }
+
+                Console.Write(SquareRoot);
+                Console.Write(" is the Square root of ");
+                Console.Write(UserNumber);
+                Console.WriteLine(".");
+
+            }
+
+        }
+    }
+
+}

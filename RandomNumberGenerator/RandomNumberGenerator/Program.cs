b48efa81f99ae041644f503afdc143ebaac87d30
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2020-05-26 18:02:57 -0500

    added notes to pointerhw from class

--- RandomNumberGenerator/RandomNumberGenerator/Program.cs
+++ RandomNumberGenerator/RandomNumberGenerator/Program.cs
@@ -11,24 +11,23 @@ namespace RandomNumberGenerator
             int HowManyNumbers;
             int i;
             bool LoopActive;
-            int[] RandomNumbers = { 0, 0, 0, 0, 0, 0, 0 };
+            int[] RandomNumbers;
 
             RandomNumber = rnd.Next(50, 101);
             LoopActive = true;
             HowManyNumbers = 0;
-            i = 1;
+            i = 0;
 
-            
-                i = 1;
-                Console.WriteLine("How many numbers would you like to generate?");
-                Console.WriteLine("(type the number, then hit enter):");
-                HowManyNumbers = (Convert.ToInt32 ( Console.ReadLine()));
-                RandomNumbers = new int[HowManyNumbers + 1];
+                
+            Console.WriteLine("How many numbers would you like to generate?");
+            Console.WriteLine("(type the number, then hit enter):");
+            HowManyNumbers = (Convert.ToInt32 ( Console.ReadLine()));
+            RandomNumbers = new int[HowManyNumbers];
 
+            Console.WriteLine(" ");
                 Console.WriteLine(" ");
-                Console.WriteLine(" ");
 
-                while (i <= HowManyNumbers)
+                while (i < HowManyNumbers)
                 {
                     RandomNumbers[i] = RandomNumber;
                     RandomNumber = rnd.Next(50, 101);
@@ -36,19 +35,28 @@ namespace RandomNumberGenerator
                 }
             while (LoopActive)
             {
-                i = 1;
-                while (i <= HowManyNumbers)
+                i = 0;
+
+                //print the numbers to the screen.
+                while (i < HowManyNumbers)
                 {
-                    Console.Write(i);
+                    Console.Write(i + 1);
                     Console.Write(". ");
                     Console.WriteLine(RandomNumbers[i]);
                     i++;
                 }
                 int NumberToChange = 0;  
 
+
+                //Change a value
                 Console.WriteLine("Which number would you like to change?");
                 Console.WriteLine("Type the Index number to the left of the number, then hit enter:");
-                NumberToChange = (Convert.ToInt32(Console.ReadLine()));
+                NumberToChange = (Convert.ToInt32(Console.ReadLine()));//NumberTochange is the index for the array "RandomNumbers"
+
+                NumberToChange--; //take the input from base 1 to base 0
+
+                //checks to make user inputs a number that is within the index range.
+                //In other words if the size of the array is 10. They shouldn't be able to enter an 11
                 if (NumberToChange < RandomNumbers.Length)
                 {
                     RandomNumbers[NumberToChange] = rnd.Next(50, 101);

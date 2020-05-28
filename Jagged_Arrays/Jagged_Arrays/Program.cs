0462a9ec2715171a81a2b24d4c99280b58158658
fleckm210 <fleckm210@gmail.com>
2020-05-28 15:54:21 -0600

    worked on project

---
+++ Jagged_Arrays/Jagged_Arrays/Program.cs
@@ -0,0 +1,74 @@
+﻿//Marshal Fleck
+//Fleckm210@gmail.com
+//This is an assignment for a system that uses jagged arrays to make a database
+using System;
+
+namespace Jagged_Arrays
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Declaring variables
+            char[][] names;
+            bool LoopActive;
+            ulong HowManyNames;
+            ulong i;
+            bool IsACharacter;
+            char EnterCheck;
+            ulong CharIndex;
+
+            //Init variables
+            LoopActive = true;
+            names = new char[10][];
+            HowManyNames = 0;
+            i = 0;
+            IsACharacter = true;
+            EnterCheck = ' ';
+            CharIndex = 0;
+
+            while (LoopActive)
+            {
+                //Introduction to the user
+                Console.WriteLine("Hello, this is an assignment for a database that uses jagged arrays.");
+                Console.WriteLine("How many names would you like to store?");
+                Console.WriteLine("Type the Number of names, then hit enter:");
+                HowManyNames = ( Convert.ToUInt64(Console.ReadLine()));
+                names = new char[HowManyNames][];
+
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+
+                while (i <= HowManyNames)
+                {
+                    while (IsACharacter)
+                    {
+
+                        names[i] = new char[HowManyNames];
+
+                        Console.Write(CharIndex + 1);
+                        Console.WriteLine(". :");
+
+                        EnterCheck += (Convert.ToChar(Console.Read()));
+                        if (EnterCheck != 13)
+                        {
+                            names[i] = new char[names[i].Length + 1];
+                            names[i][CharIndex] = EnterCheck;
+                            CharIndex++;
+                        }
+                        else
+                        {
+                            IsACharacter = false;
+                        }
+                            
+                        
+                    }
+                    Console.Write(names[i] [i]);
+
+
+                    i++;
+                }
+            }
+        }
+    }
+}
--- RandomNumberGenerator/RandomNumberGenerator/Program.cs
+++ RandomNumberGenerator/RandomNumberGenerator/Program.cs
@@ -5,74 +5,107 @@ namespace RandomNumberGenerator
     {
         public static void Main(string[] args)
         {
-
-            Random rnd = new Random();
-            int RandomNumber;
-            int HowManyNumbers;
-            int i;
-            bool LoopActive;
-            int[] RandomNumbers;
-
-            RandomNumber = rnd.Next(50, 101);
-            LoopActive = true;
-            HowManyNumbers = 0;
-            i = 0;
-
-                
-            Console.WriteLine("How many numbers would you like to generate?");
-            Console.WriteLine("(type the number, then hit enter):");
-            HowManyNumbers = (Convert.ToInt32 ( Console.ReadLine()));
-            RandomNumbers = new int[HowManyNumbers];
-
-            Console.WriteLine(" ");
-                Console.WriteLine(" ");
-
-                while (i < HowManyNumbers)
-                {
-                    RandomNumbers[i] = RandomNumber;
-                    RandomNumber = rnd.Next(50, 101);
-                    i++;
-                }
-            while (LoopActive)
+            unsafe
             {
+
+                long[] ChangeRandomNumber;
+                Random rnd = new Random();
+                long RandomNumber;
+                int HowManyNumbers;
+                int i;
+                bool LoopActive;
+                bool LoopActive1;
+                long[] RandomNumbers;
+                string YesOrNo;
+
+                RandomNumber = rnd.Next(50, 101);
+                LoopActive = true;
+                LoopActive1 = true;
+                HowManyNumbers = 0;
                 i = 0;
+                YesOrNo = " ";
 
-                //print the numbers to the screen.
-                while (i < HowManyNumbers)
+                while (LoopActive1)
                 {
-                    Console.Write(i + 1);
-                    Console.Write(". ");
-                    Console.WriteLine(RandomNumbers[i]);
-                    i++;
+                    Console.WriteLine("How many numbers would you like to generate?");
+                    Console.WriteLine("(type the number, then hit enter):");
+                    HowManyNumbers = (Convert.ToInt32(Console.ReadLine()));
+                    RandomNumbers = new long[HowManyNumbers];
+                    ChangeRandomNumber = RandomNumbers;
+
+                    Console.WriteLine(" ");
+                    Console.WriteLine(" ");
+
+                    while (i < HowManyNumbers)
+                    {
+                        RandomNumbers[i] = RandomNumber;
+                        RandomNumber = rnd.Next(50, 101);
+                        i++;
+                    }
+                    while (LoopActive)
+                    {
+                        i = 0;
+
+                        //print the numbers to the screen.
+                        while (i < HowManyNumbers)
+                        {
+                            Console.Write(i + 1);
+                            Console.Write(". ");
+                            Console.WriteLine(RandomNumbers[i]);
+                            i++;
+                        }
+                        int NumberToChange = 0;
+
+
+                        //Change a value
+                        Console.WriteLine("Which number would you like to change?");
+                        Console.WriteLine("Type the Index number to the left of the number, then hit enter:");
+                        NumberToChange = (Convert.ToInt32(Console.ReadLine()));//NumberTochange is the index for the array "RandomNumbers"
+
+                        NumberToChange--; //take the input from base 1 to base 0
+
+                        //checks to make user inputs a number that is within the index range.
+                        //In other words if the size of the array is 10. They shouldn't be able to enter an 11
+                        if (NumberToChange < RandomNumbers.Length)
+                        {
+                            ChangeRandomNumber[NumberToChange] = rnd.Next(50, 101);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Sorry, that number does not exist.");
+                        }
+
+
+                        Console.WriteLine("Would you like to keep changing numbers, or would you like to generate new ones?");
+                        Console.WriteLine("'Yes' to keep changing, 'no' to generate new ones:");
+                        YesOrNo = Console.ReadLine();
+
+                        while (LoopActive)
+                        {
+
+                            if (YesOrNo == "no")
+                            {
+                                LoopActive = false;
+                            }
+                            else if (YesOrNo == "yes")
+                            {
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Sorry, try that again:");
+                            }
+
+                        }
+
+
+
+
+                    }
                 }
-                int NumberToChange = 0;  
-
-
-                //Change a value
-                Console.WriteLine("Which number would you like to change?");
-                Console.WriteLine("Type the Index number to the left of the number, then hit enter:");
-                NumberToChange = (Convert.ToInt32(Console.ReadLine()));//NumberTochange is the index for the array "RandomNumbers"
-
-                NumberToChange--; //take the input from base 1 to base 0
-
-                //checks to make user inputs a number that is within the index range.
-                //In other words if the size of the array is 10. They shouldn't be able to enter an 11
-                if (NumberToChange < RandomNumbers.Length)
-                {
-                    RandomNumbers[NumberToChange] = rnd.Next(50, 101);
-                }
-                else
-                {
-                    Console.WriteLine("Sorry, that number does not exist.");
-                }
-                
-
-                Console.WriteLine("Press any key to continue:");
-                Console.ReadKey();
-
-
-
             }
+
+            
             
         }
     }

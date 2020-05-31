f2334c5cfd66a19b08c29fb29dc846e85989d41d
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2020-05-30 20:35:02 -0500

    Finished making lessons 12&13

---
+++ InClassLesson12_ErrorCatch/InClassLesson12_ErrorCatch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InClassLesson12_ErrorCatch
+{
+    class Program
+    {
+        //You can create an error message using the following code
+        //throw new Exception("Here's an error");
+
+        static void Main(string[] args)
+        {
+            int x;
+
+            //example of when an error can occur
+            Console.WriteLine("Type a number and it works. But try typing 'hello':");
+            x = Convert.ToInt32(Console.ReadLine());
+
+
+            //Do an example of try catch using
+            //x = Convert.ToInt32(Console.ReadLine());
+
+            //show how to catch an error
+
+            //shows how code gets skipped when an error is caught
+
+            //show how catch can get passed an Exception parameter
+
+            //Call SomeFunction here that shows how to create an error using throw
+
+            //Give an example on how throws can be used with try-catch
+
+
+            //Go over sleeps for fun
+            //Console.WriteLine("One");
+            //1000 miliseconds is 1 second
+            //Thread.Sleep(1000);
+
+
+
+        }
+    }
+}
---
+++ InClassLesson13_Recursion/InClassLesson13_Recursion/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace InClassLesson13_Recursion
+{
+    class Program
+    {
+        //use this magic line to create a datatype for functions.
+        //parameters and return value must match or it can't be passed into that type
+        //Think of it as a datatype definition
+        //private delegate int mathStuff(int num1, int num2);
+
+
+        static void Main(string[] args)
+        {
+            //Create Test recurrsion function showing how a function can call itself
+
+            //Create a Count function
+
+            //step through code to show how it acts like a function
+
+            //print x before the call and after to show how it acts like a stack
+
+            //Explain the stack... how many x varibles exist in memory??
+
+            //write the fibancii squence using recurrsion
+
+
+
+
+            //Call back functions (method means function)
+
+            //make a calculator function that takes in a function as a parameter
+
+            //make an add and multi function to show the choice
+
+            //example of how another function can actually be used to set the function
+
+        }
+    }
+}

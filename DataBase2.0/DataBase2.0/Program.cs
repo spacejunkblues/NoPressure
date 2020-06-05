607ae597fbd7c95b9ce33a905c2e017bf67c8a2f
fleckm210 <fleckm210@gmail.com>
2020-06-04 20:19:22 -0600

    finished database2.0

--- DataBase2.0/DataBase2.0/Program.cs
+++ DataBase2.0/DataBase2.0/Program.cs
@@ -7,81 +7,127 @@ namespace DataBase2
 {
     class MainClass
     {
-        //Adds a name the Names array
-        static void AddName(ref string[]Names)
+        static void AddNamesForTesting(ref string[] names)
         {
-            //Getting Closest blank name using the search function
-            int i = 0;
-            i = Search(Names, "");
-            string NewName = "";
-            bool AllIsOkay = true;
 
-            while (true)
+            for(int i=0;i<10;i++)
             {
-                //Asking user for Information on the added person
-                Console.WriteLine("What is the persons name?");
-                Console.WriteLine("Remember, you can do First and last name,");
-                Console.WriteLine("Or just first name. Hit enter when you are done:");
-                NewName = Console.ReadLine();
+                names[i] = "Name" + (i +1);
+            }
+        
+        }//End AddNamesForTesting
 
-                //Clears space
-                Console.WriteLine("");
-                Console.WriteLine("");
+        //Adds a name the Names array
+        static void AddName(ref string[] Names)
+        {
+            int i = 10;
+            int NotDuplite = 10;
 
-                for (int index = 0; index < 10; index++)
+            //Getting Closest blank name using the search function
+            try
+            {
+                i = Search(Names, (null));
+            }
+            catch(Exception errorVaraible)
+            {
+                if(errorVaraible.Message == "Error")
                 {
-                    if (NewName == Names[index])
-                    {
-                        AllIsOkay = false;
-                    }
+                    
                 }
-                if (AllIsOkay)
-                {
-                    break;
-                }
-
             }
 
-            //Displays the added name
-            Console.Write("Okay, ");
-            Console.Write(Names[i]);
-            Console.WriteLine(" has been added to the DataBase.");
+            //If the search function comes up empty
+            if (i != 10)
+            {
+
+                string NewName = "";
+                bool AllIsOkay = false;
+
+                //Asks the user for information
+                Console.WriteLine("What is the persons name?");
+                NewName = Console.ReadLine();
+
+                //Uses the search function to make sure there is no duplicates
+                try
+                {
+                    NotDuplite = Search(Names, NewName);
+                }
+                catch (Exception errorVaraible)
+                {
+                    if (errorVaraible.Message == " ")
+                    {
+
+                    }
+                }
+
+
+
+                if (NotDuplite == 10)
+                {
+                    AllIsOkay = true;
+                }
+
+
+                if (AllIsOkay)
+                {
+                    //Displays the added name
+                    Console.Write("Okay, ");
+                    Console.Write(NewName);
+                    Console.WriteLine(" has been added to the DataBase.");
+
+                    Names[i] = NewName;
+
+                    //Clears space
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, that name is already in the database.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sorry, the database is full.");
+                Console.WriteLine("Use the delete function to clear some room.");
+            }
 
             //Clears space
             Console.WriteLine("");
             Console.WriteLine("");
 
-            //This is the function that displays the users options
-            static void DisplayOptions()
-            {
-                //Tells the user what they can do
-                Console.WriteLine("You can 'add' a person.");
-                Console.WriteLine("You can 'delete' a person.");
-                Console.WriteLine("You can 'view' all names.");
-                Console.WriteLine("Or you can 'search' for a person:");
-             }
+
+        }//End of AddName function
 
         //Determines the closest blank name
-        static int Search(string[]Names, string WhatToSearchFor)
+        static int Search(string[] Names, string WhatToSearchFor)
         {
-            int ClosestNumber = 0;
-            for (int i = 9;i < -1; i--)
+            //Determines the closest answer
+            int ClosestNumber = 10;
+            for (int i = 9; i > -1; i--)
             {
                 if (Names[i] == WhatToSearchFor)
                 {
                     ClosestNumber = i;
                 }
             }
+            
 
-
+            if (ClosestNumber == 10)
+            {
+                throw new Exception("Error");
+            }
 
             return ClosestNumber;
-        }
+
+        }//End of Search function
+
 
         static string[] Delete(string[] Names, ref bool IsAnError)
         {
             int WhichNameToDelete = 0;
             bool IsAName = false;
+            bool IfTyped = false;
             IsAnError = true;
 
             //Determines if names[index] does not equal nothing
@@ -95,6 +141,8 @@ namespace DataBase2
                     IsAName = true;
                 }
             }
+
+            //IsName will only be true if there is a name in the database
             if (IsAName)
             {
                 //Clears space
@@ -107,12 +155,37 @@ namespace DataBase2
                 try
                 {
                     WhichNameToDelete = Convert.ToInt32(Console.ReadLine());
-                    IsAnError = false;
-                    Names[WhichNameToDelete + 1] = (null);
-                }   
-                catch (Exception)
+
+                    //Clears space
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+
+                    if (Names[WhichNameToDelete - 1] != (null))
+                    {
+
+                        IfTyped = true;
+                        IsAnError = false;
+                        Names[WhichNameToDelete - 1] = (null);
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                    }
+                }
+                catch(Exception e)
                 {
-                    
+                    if (e.Message == "Index was outside the bounds of the array.")
+                    {
+                        Console.WriteLine("Sorry, that number does not exist.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, it has to be a number.");
+                    }
+
+                    //Clears space
+                    Console.WriteLine("");
+                    Console.WriteLine("");
                 }
             }
             else
@@ -123,12 +196,12 @@ namespace DataBase2
             //Clears space
             Console.WriteLine(" ");
             Console.WriteLine(" ");
-            
+
 
             return Names;
-        }
+        }//end of delete function
 
-        static void ViewNames(string[]Names, ref bool IsNames)
+        static void ViewNames(string[] Names, ref bool IsNames)
         {
             //If There is names in the database
             IsNames = false;
@@ -155,8 +228,19 @@ namespace DataBase2
                 Console.WriteLine("Press any key to continue:");
                 Console.ReadKey();
             }
-          
+
+        }//end of viewnames function
+
+        static void DisplayOptions()
+        {
+            Console.WriteLine("You can 'add' a name,");
+            Console.WriteLine("You can 'delete' a name,");
+            Console.WriteLine("You can 'view' all names,");
+
         }
+            
+
+
 
 
             //Start of our program
@@ -180,8 +264,11 @@ namespace DataBase2
             WhatToSearchFor = " ";
             SearchIndex = 0;
 
+            //This function init's the names array. Used for testing only
+            AddNamesForTesting(ref Names);
+
             //Introduction to the user
-            Console.WriteLine("Hello. This is a databse that can store the names");
+            Console.WriteLine("Hello. This is a database that can store the names");
             Console.WriteLine("for up to 10 people.");
             Console.WriteLine("Make sure to type all lower case, and to hit enter.");
 
@@ -189,7 +276,7 @@ namespace DataBase2
             Console.WriteLine(" ");
             Console.WriteLine(" ");
 
-            //While the main program is runninf
+            //While the main program is running
             while (MainMenuActive)
             {
                 //Calls The function that displays options
@@ -205,24 +292,26 @@ namespace DataBase2
                 {
                     //Calls the function that adds a name to the Names array
                     AddName(ref Names);
+                    ViewNames(Names, ref IsNames);
+                    
                 }
 
                 //If the user wants to delete a person
                 if (WhatTheUserWantsToDo == "delete")
                 {
-                    while (true)
+                    try
                     {
                         Names = Delete(Names, ref IsAnError);
-                        if (IsAnError)
-                        {
-                            Console.WriteLine("Sorry, you have to type a number.");
-                        }
-                        else
-                        {
-                            break;
-                        }
                     }
-                    
+                    catch(Exception Error)
+                    {
+                        Console.WriteLine(Error.Message);
+
+                        //Clears space
+                        Console.WriteLine(" ");
+                        Console.WriteLine(" ");
+                    }
+           
                 }
 
                 //If the user wants to view all the names
@@ -252,13 +341,27 @@ namespace DataBase2
                         Console.WriteLine("Who would you like to search for?");
                         WhatToSearchFor = Console.ReadLine();
 
+                        
+
                         //Clears space
                         Console.WriteLine(" ");
                         Console.WriteLine(" ");
 
+                    try
+                    {
+                        //Calls the search function
+                        SearchIndex = Search(Names, WhatToSearchFor);
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Sorry, there is no one in the database that has that name.");
 
-                    //Calls the search function
-                    SearchIndex = Search(Names, WhatToSearchFor);
+                        //Clears space
+                        Console.WriteLine(" ");
+                        Console.WriteLine(" ");
+
+                        
+                    }
 
                     if (Names[SearchIndex] != (null) )
                     {
@@ -270,10 +373,6 @@ namespace DataBase2
                         Console.WriteLine(" ");
                         Console.WriteLine(" ");
                     }
-                    else
-                    {
-                        Console.WriteLine("Sorry, there is no one in the database that has that name. ");
-                    }
 
                 }
 
@@ -281,6 +380,6 @@ namespace DataBase2
                 Console.WriteLine(" ");
                 Console.WriteLine(" ");
             }
-        }
+        }//End of main function
     }
 }

61c5e6425489b9feea23018f867b6b5a7aa13db0
fleckm210 <fleckm210@gmail.com>
2020-06-10 11:22:10 -0600

    created tower project

---
+++ towerproblem/towerproblem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace towerproblem
+{
+    class MainClass
+    {
+        static void towersolution(int HowManyRings)
+        {
+            //Declaring variables
+            string[]pegs;
+            int[] rings;
+
+            //Init variables
+            pegs = new string[3];
+            rings = new int[HowManyRings];
+
+            Console.WriteLine();
+        }
+
+        public static void Main(string[] args)
+        {
+                    
+        }
+    }
+}

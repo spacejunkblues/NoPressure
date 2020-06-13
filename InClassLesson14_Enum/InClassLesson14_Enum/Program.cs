aa0a39c7aa20bc18929add8cc74ea98cbc0c80ac
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2020-06-12 20:23:17 -0500

    made generic and first OOP lesson

---
+++ InClassLesson14_Enum/InClassLesson14_Enum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+//must add this using:
+//using System.Collections.Generic;
+
+//This program is mis-named. We aren't doing enums yet, flex to generic types and vars
+
+namespace InClassLesson14_Enum
+{
+    
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //introduce <> Operator
+
+            //Show how it can be used in the function, as a parameter, and as a return. Note: All the T's are the same datatype!
+
+            //Make swap function
+
+            //show how it will figure it out
+
+            //show how to force it to be a data type
+
+            //type the delegate statment up above. Make it an int.
+
+            //create the callback function
+
+            //show it works
+
+            //create a second callback function using a different datatype
+
+            //use var
+
+
+
+        }
+    }
+}
---
+++ InClassLesson15_Objects/InClassLesson15_Objects/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace InClassLesson15_Objects
+{
+    //create a class with two variables
+
+    //add public to one variable to access the variable
+
+    //add a method to the class (birthday)
+
+    //explain how varaibles can be used in methods scope
+
+    //create getter for age
+
+    //explain private vs public
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create the object of the class. with name and age
+
+            //explain why we need to use new. show memory on big board
+
+            //set a name value using '.'
+
+            //get a value, run f10. Show what memory looks like
+            //show how age is private.
+
+            //add array to class. Explain how memory is being pointed
+
+            //set array
+
+            //change index 0 of the array using the class
+
+            //change index 1 of a local array pointing to the same spot
+
+            //set one object equal to another
+
+            //show in f10 how the & of one is actually storing a pointer value.
+
+            //create a new spot in memory for marshy(new object). What will happen to the array inside?
+
+            //set new object's array equal to old objects array
+
+            //change index 2 of the new spot. Do they all change? Or just one?
+
+            //Explain static vs non-static (show how no object is needed for static)
+
+ 
+        }
+    }
+}

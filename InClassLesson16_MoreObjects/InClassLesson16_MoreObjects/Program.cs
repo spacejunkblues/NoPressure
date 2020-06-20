79aff82c22fb6c81c7501fe3791b47735e0e8aa7
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2020-06-20 18:09:01 -0500

    lessons ready for more objects

---
+++ InClassLesson16_MoreObjects/InClassLesson16_MoreObjects/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace InClassLesson16_MoreObjects
+{
+    //create a class
+
+    //create a constructor
+
+    //explain that there is a destructor, but it runs automatticly
+
+    //pass the contructor a varible
+
+    //show inherietence. Use animal class
+
+    //explain protected vs private
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //show how a contructor will set name
+
+            //show how adding a parent will add attributes
+
+            //change the public attribute in the parent class to private. Show errors
+            
+            //Change it to protected. show erros
+        }
+    }
+}

6c6de1d2d76a7ac98f0c068c435f596878e7d97f
fleckm210 <fleckm210@gmail.com>
2020-06-21 20:10:37 -0600

    Completed Project stringobject

--- stringobject/stringobject/Program.cs
+++ stringobject/stringobject/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿//This an Assignment for a Char Array simulating a string
+using System;
 
 namespace stringobject
 {
+    //Mstring is a Char Array simulating a string
     class Mstring
     {
         //Created 2 private variables, CharArray, and Size
@@ -12,34 +14,75 @@ namespace stringobject
         //SetString sets CharArray to mstring
         public void SetString(char[]mstring)
         {
+            //Assigns CharArray Length to mstring Length
             CharArray = new char[mstring.Length];
-            for (long i = 0; i < mstring.Length; i++)
-            {
-                CharArray[i] = mstring[i];
-            }
+            Size = CharArray.Length;
+
+            //Assigns CharArray to mstring using AssignArrays
+            AssignArrays(ref CharArray, mstring, mstring.Length);
         }
 
         //GetString returns the CharArray
-        char[] GetString()
+        public char[] GetString()
         {
             //Copy of CharArray
             char[] clone = new char[CharArray.Length];
 
             //Assigns copy of CharArray
-            for (long i = 0; i < CharArray.Length; i++)
-            {
-                clone[i] = CharArray[i];
-            }
+            AssignArrays(ref clone, CharArray, Size);
 
+            //Returns a copy of chararray to MainClass
             return clone;
         }
+
+        //AppendString adds 2 char arrays to each other
+        public void AppendString(char[]Addition)
+        {
+            //Created a new char array as a placeholder
+            char[] PlaceHolder = new char[CharArray.Length + Addition.Length];
+
+            //Assign PlaceHolder to CharArray using AssignArrays
+            AssignArrays(ref PlaceHolder, CharArray, Size);
+
+            //Declares index for Addition
+            long index = 0;
+
+            //Assigns Addition to the rest of PlaceHolder
+            for (long i = CharArray.Length; i < PlaceHolder.Length; i++)
+            {
+                //Assigns the rest of PlaceHolder to Addition
+                PlaceHolder[i] = Addition[index];
+
+                //Increments Index for Addition
+                index++;
+            }
+
+            //Creates a new Array the size of PlaceHolder
+            CharArray = new char[PlaceHolder.Length];
+            Size = CharArray.Length;
+
+            //Assigns CharArray to PlaceHolder using AssignArrays
+            AssignArrays(ref CharArray, PlaceHolder, Size);
+        }
+
+        //Assign Arrays to Each Other
+        void AssignArrays(ref char[]Array1, char[]Array2, long Index)
+        {
+            //Assigns one Array to another
+            for (long i = 0; i < Index; i++)
+            {
+                //Assigns Arrays
+                Array1[i] = Array2[i];
+
+            }
+        }
+
     }
 
     class MainClass
     {
         public static void Main(string[] args)
         {
-            char[] chararrey = new char[10];
             
         }
     }

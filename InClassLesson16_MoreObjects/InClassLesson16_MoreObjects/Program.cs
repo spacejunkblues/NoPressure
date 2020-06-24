d9d8e637d1d0b9c7d5927b5a0b8a272a98085f40
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2020-06-23 20:19:45 -0500

    completed moreobjects lesson

--- InClassLesson16_MoreObjects/InClassLesson16_MoreObjects/Program.cs
+++ InClassLesson16_MoreObjects/InClassLesson16_MoreObjects/Program.cs
@@ -2,13 +2,68 @@
 
 namespace InClassLesson16_MoreObjects
 {
-    //create a class
+    class Animal
+    {
+        protected string type;
 
-    //create a constructor
+        //run
+
+        //eat
+
+        //do animal stuff
+
+        
+    }
+
+
+    //create a class
+    class Person:Animal
+    {
+        public string name;
+        private int age;
+
+    //pass the contructor a varible
+        public Person(string StartName, int StartAge)
+        {
+            name = StartName;
+            age = StartAge;
+        }
+        //create a constructor
+        public Person()
+        {
+        }
+
+        public Person(string StartType)
+        {
+            type = StartType;
+        }
+
+        static public bool operator > (Person p1, Person p2)
+        {
+            return p1.age > p2.age;
+        }
+        static public bool operator <(Person p1, Person p2)
+        {
+            return p1.age < p2.age;
+        }
+
+        static public Person operator ++(Person p1)
+        {
+            p1.age++;
+
+            return p1;
+        }
+
+        static public void test()
+        {
+            Console.WriteLine("test");
+        }
+
+    }
+
 
     //explain that there is a destructor, but it runs automatticly
 
-    //pass the contructor a varible
 
     //show inherietence. Use animal class
 
@@ -20,12 +75,41 @@ namespace InClassLesson16_MoreObjects
         static void Main(string[] args)
         {
             //show how a contructor will set name
+            Person uncle = new Person("Uncel", 40);
+
+            uncle.name = "louie";
+
+            Person marshy;
+
+            marshy = new Person("marshy", 36);
 
             //show how adding a parent will add attributes
+           // uncle.type = "dog";
+
+            Animal mika = new Animal();
+
+            // mika.type = "cat";
 
             //change the public attribute in the parent class to private. Show errors
-            
+
             //Change it to protected. show erros
+
+            if(uncle > marshy)
+            {
+             Console.WriteLine("Uncle is older");
+            }
+
+            marshy++;
+
+            Person.test();
+
+            marshy = uncle;
+
+            if(uncle == marshy)
+            {
+                Console.WriteLine("Same");
+            }
+       
         }
     }
 }

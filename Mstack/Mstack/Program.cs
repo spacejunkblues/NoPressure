6e05b386a1ffdd0ed436755c3e2a10bd6da20646
fleckm210 <fleckm210@gmail.com>
2020-06-26 08:50:20 -0600

    CreatedMstack

---
+++ Mstack/Mstack/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Mstack
+{
+    //Class Mstask can store other objects
+    class mstack
+    {
+        //Created two variables, Data and Next
+        //Data holds an int variable
+        int Data;
+
+        //Data is the next one in the mstack
+        mstack Next;
+
+        //Push Adds one mstack to the mstack
+        public void Push(int NewStack)
+        {
+
+        }
+
+        //Pop takes the top of the stask and returns
+        public int Pop()
+        {
+
+            return 1;
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

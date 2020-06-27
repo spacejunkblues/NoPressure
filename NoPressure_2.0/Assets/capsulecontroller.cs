697069ac14ff3a4680d2e8d3a34b3f2e4eaf99e3
fleckm210 <fleckm210@gmail.com>
2020-06-26 18:24:58 -0600

    ceated unity scene platformer

---
+++ NoPressure_2.0/Assets/capsulecontroller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class capsulecontroller : MonoBehaviour
+{
+    bool onground;
+    public Rigidbody rb;
+    public Transform tf;
+    public float Speed;
+    int counter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        onground = true;
+        Speed = 0.05f;
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter += 1;
+        if (Input.GetKey(KeyCode.W))
+        {
+            tf.position = new Vector3(tf.position.x, tf.position.y, tf.position.z + Speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            tf.position = new Vector3(tf.position.x - Speed, tf.position.y, tf.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            tf.position = new Vector3(tf.position.x, tf.position.y, tf.position.z - Speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            tf.position = new Vector3(tf.position.x + Speed, tf.position.y, tf.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.Space) && onground && counter > 3)
+        {
+            rb.AddForce(0, 130, 0);
+            onground = false;
+            counter = 0;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        onground = true;
+    }
+}

773bf899234d221bde7ab94fe0a8ec0db9935c8c
fleckm210 <fleckm210@gmail.com>
2020-07-01 16:42:43 -0600

    created project

---
+++ AdvancedDatabase/AdvancedDatabase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvancedDatabase
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

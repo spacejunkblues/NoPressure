9c5bb99de9e07cc19c6a30f462b36ef42d48eb65
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2020-06-30 21:47:59 -0500

    comp;eted mstack

---
+++ InClassLesson18_SaveFiles/InClassLesson18_SaveFiles/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+
+namespace InClassLesson18_SaveFiles
+{
+    class Program
+    {
+         //define where we are going to save it.
+        static string path = @"C:\Users\User\Documents\GitHub\NoPressure\InClassLesson18_SaveFiles\test.txt";
+
+        static void Save()
+        {
+            //define our message that will be saved
+            string message = "Hello Marshy!!!\n";
+
+
+            //save into a file
+            System.IO.File.WriteAllText(path, message);
+
+        }
+
+
+        static string Load()
+        {
+
+            return System.IO.File.ReadAllText(path);
+        }
+
+        static void Main(string[] args)
+        {
+            Save();
+            //Console.WriteLine(Load());
+
+
+            //define our message that will be saved
+            string message = "\nHello again Marshy!!!\n" ;
+
+            string[] msg = { "dog", "cat" };
+
+
+
+
+            //save into a file
+            System.IO.File.WriteAllLines(path, msg);
+
+            Console.WriteLine( System.IO.File.ReadAllLines(path));
+            //System.IO.File.AppendAllText(path, message);
+            //System.IO.File.AppendAllText(path, message);
+
+
+
+            //System.IO.File.WriteAllText(path, "using enconding");
+        }
+    }
+}

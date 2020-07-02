71b03dcac82a8c5b6ee15a4c90de0aa2cf5a31a5
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2020-07-01 20:19:48 -0500

    enum class prepped

---
+++ InClassLesson19_Enum/InClassLesson19_Enum/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Transactions;
+
+namespace InClassLesson19_Enum
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            //introduc the IEnum class
+
+
+
+            //show how string inhereits the IEnum class
+
+            //set the GetEnumerator of the string to get a handle from the data structure
+
+
+            //use next to get the first element
+    
+
+            //use current to show what's in the data structure
+ 
+
+            //use next to get the next element
+ 
+
+            //show a foreach
+            //note: it's just using the GetEnumerator function to step through the elements
+
+
+            //foreach ONLY works with IEnumerted class
+
+            //create an mstack and show how you cant' use foreach
+
+            //create a simple example using IEnumator<> return type and yield return (look up notes in enum folder)
+
+            //*****INCLASS EXERCISE!!!! Copy mstack into it's own cs file. Import into this project and add it's namespace  ********
+            //*****       go into mstack and overload the GetEnumerator() function to enable foreach                        ********
+
+
+            
+        }
+    }
+}

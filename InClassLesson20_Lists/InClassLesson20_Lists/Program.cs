d189314387cd59962fff7dea865f0b5ce53ae3cd
spacejunkblues <53051074+spacejunkblues@users.noreply.github.com>
2020-07-05 12:56:02 -0500

    ready for list and IO class

---
+++ InClassLesson20_Lists/InClassLesson20_Lists/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+
+namespace InClassLesson20_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a List<>
+            //note that it's a generic class
+
+            //note that the object contains GetEnumerator
+
+            //Add to the list
+
+            //print the list
+            //note that it doesn't print directly with writeline (to be expected)
+
+            //show how the [] operator was overloaded to work with lists
+
+            //use with foreach
+
+            //note all the other methods like ToArray, Find, etc
+
+            //Remove first occurance or RemoveAt
+
+
+            //Create a Dictionary<,>
+
+            //demo key value pairs with ID's and names
+
+            //show how dict overloaded the index to be used with key's
+
+            //can still print one at a time with foreach
+            //notice how each element in the data structure is a key,value pair.
+            //can use .Key or .Value
+
+            //can use ElementAt to get index in the order that they were entered
+
+        }
+    }
+}
---
+++ InClassLesson21_IO/InClassLesson21_IO/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace InClassLesson21_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //write to the screen
+            Console.WriteLine("Hello World!");
+
+            //move the cursor
+            Console.SetCursorPosition(0, 0);
+
+            //Change a single letter
+            Console.Write("F");
+
+            //clear the console
+            Console.Clear();
+
+            //You can change the color of the console
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            //use clear to write the new color to the entire screen
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+
+            //Can reset
+            Console.ResetColor();
+            Console.Clear();
+
+            //get WindowHeight and WindowWidth
+            int maxheight =Console.WindowHeight;
+            int maxwidth = Console.WindowWidth;
+
+            for (int i = 1; i <= maxheight; i++)
+                Console.WriteLine(i);
+
+            //print a '*' in the middle of the console
+            Console.SetCursorPosition(maxwidth / 2, maxheight / 2);
+            Console.Write("*");
+
+            //Use ReadyKey to move the character. Use var to get the return and figure out how to check for UpArrow
+
+
+        }
+    }
+}

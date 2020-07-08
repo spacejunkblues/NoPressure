b219cbf9dfd3ee03378f47dd0fe1399470c970dc
fleckm210 <fleckm210@gmail.com>
2020-07-08 08:42:04 -0600

    created project

---
+++ Mdict/Mdict/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Mdict
+{
+    class Mdict<T>
+    {
+        public Mdict<T> value;
+
+        public T name;
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}

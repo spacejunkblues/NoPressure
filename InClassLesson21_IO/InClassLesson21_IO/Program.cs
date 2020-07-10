4bf88bd0aa5b4a81c629a64dcf6272c24c259942
fleckm210 <fleckm210@gmail.com>
2020-07-09 19:58:00 -0600

    started maze

--- InClassLesson21_IO/InClassLesson21_IO/Program.cs
+++ InClassLesson21_IO/InClassLesson21_IO/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Web;
 
 namespace InClassLesson21_IO
 {
@@ -6,6 +8,10 @@ namespace InClassLesson21_IO
     {
         static void Main(string[] args)
         {
+            var j = Console.ReadKey();
+
+            
+
             //write to the screen
             Console.WriteLine("Hello World!");
 
@@ -31,18 +37,99 @@ namespace InClassLesson21_IO
             Console.Clear();
 
             //get WindowHeight and WindowWidth
-            int maxheight =Console.WindowHeight;
+            int maxheight = Console.WindowHeight;
             int maxwidth = Console.WindowWidth;
 
             for (int i = 1; i <= maxheight; i++)
                 Console.WriteLine(i);
 
+            Console.Clear();
+
             //print a '*' in the middle of the console
             Console.SetCursorPosition(maxwidth / 2, maxheight / 2);
             Console.Write("*");
 
-            //Use ReadyKey to move the character. Use var to get the return and figure out how to check for UpArrow
+            int i1 = maxwidth / 2;
+            int i2 = maxheight / 2;
 
+
+            //Use ReadyKey to move the character. Use var to get the return and figure out how to check for UpArrow
+            while (true)
+            {
+
+                //up
+                ConsoleKeyInfo i = Console.ReadKey();
+                if (i2 > 1)
+                {
+                    if (i.Key == ConsoleKey.UpArrow)
+                    {
+                        Console.Clear();
+
+                        Console.SetCursorPosition(i1, i2 - 1);
+
+                        i2 -= 1;
+
+                        Console.Write("*");
+                    }
+                }
+
+                //down
+                if (i2 < 19)
+                {
+                    if (i.Key == ConsoleKey.DownArrow)
+                    {
+                        Console.Clear();
+
+                        Console.SetCursorPosition(i1, i2 + 1);
+
+                        i2 += 1;
+
+                        Console.Write("*");
+                    }
+                }
+
+                //left
+                if (i1 > 0)
+                {
+                    if (i.Key == ConsoleKey.LeftArrow)
+                    {
+                        Console.Clear();
+
+                        Console.SetCursorPosition(i1 - 1, i2);
+
+                        i1 -= 1;
+
+                        Console.Write("*");
+                    }
+                }
+
+                //right
+                if (i1 < 80)
+                {
+                    if (i.Key == ConsoleKey.RightArrow)
+                    {
+                        Console.Clear();
+
+                        Console.SetCursorPosition(i1 + 1, i2);
+
+                        i1 += 1;
+
+                        Console.Write("*");
+                    }
+                }
+
+                Random m = new Random();
+
+                //Create maze
+                for (int index = 0; index < 200; index++)
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+                    Console.SetCursorPosition(i1 + m.Next(0, 80), i2 + m.Next(0, 26));
+                    Console.Write(m.Next(0, 20));
+                }
+            }
 
         }
     }

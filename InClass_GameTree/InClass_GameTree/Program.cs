a3c3526f07a3c837c659565bd4ab04beb354e268
fleckm210 <fleckm210@gmail.com>
2020-08-14 08:57:09 -0600

    created wave effect, and made tic toe bot's moves visible

--- InClass_GameTree/InClass_GameTree/Program.cs
+++ InClass_GameTree/InClass_GameTree/Program.cs
@@ -372,6 +372,9 @@ namespace InClass_GameTree
 
         public static int TryMediumPiece(Board curbrd, char player, int HowManyMoreLayers, ref Piece PieceToPlace)
         {
+            curbrd.Print();
+            curbrd.Print2();
+
             //Assign Useful variable
             Piece EP = new Piece();
 
@@ -393,11 +396,13 @@ namespace InClass_GameTree
             //loop through blanks using hvalue
             foreach (var n in curbrd.blanks)
             {
+                Thread.Sleep(100);
+
                 //Reset Fboard
                 FBoard = new Board(curbrd);
 
                 //find the value in placing the current piece
-                FBoard.PlacePiece(new Piece(n.x, n.y, player), true);
+                FBoard.PlacePiece(new Piece(n.x, n.y, player), false);
 
                 //if that piece will win, return that piece
                 if (FBoard.win(player))
@@ -424,12 +429,14 @@ namespace InClass_GameTree
                     if (IfFirstLoop)
                     {
                         Hvalue = TryMediumPiece(FBoard, GetOtherPlayer(player), HowManyMoreLayers - 1, ref EP);
+                        Thread.Sleep(100);
                         EHV = Hvalue;
                         IfFirstLoop = false;
                     }
                     else 
                     {
                         EHV = TryMediumPiece(FBoard, GetOtherPlayer(player), HowManyMoreLayers - 1, ref EP);
+                        Thread.Sleep(100);
                     }
 
                 }
@@ -454,6 +461,8 @@ namespace InClass_GameTree
                     Hvalue = EHV;
                 }
 
+                FBoard.Print();
+                FBoard.Print2();
 
             }
 
@@ -464,6 +473,7 @@ namespace InClass_GameTree
             PieceToPlace = new Piece(HHvalue.x, HHvalue.y, HHvalue.Appearnace);
 
             FBoard.PlacePiece(PieceToPlace, false);
+            Thread.Sleep(100);
 
             return HValue(FBoard);
         }
@@ -768,8 +778,8 @@ namespace InClass_GameTree
             int x;//user input locaiotns
             int y;
             char winner = 'N';//N for none, other options are X and O
-            int howmanylayers = 4;
-            int whichturn = 0;            
+            int howmanylayers = 8;
+            int whichturn = 0;
 
             do
             {
---
+++ WaveEffect/WaveEffect/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace WaveEffect
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            for (int j = 1; j < 3; j++)
+            {
+                Console.SetCursorPosition(0, 0);
+                for (int i = 0; i < 2000; i++)
+                {
+                    Console.Write(j);
+                    Thread.Sleep(5);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
